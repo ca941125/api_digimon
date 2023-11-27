138050f9b105d8ed1ed96bc64acf10edc4792d6d
Carolina Aguirre <carolinaaguirre12@gmail.com>
2023-11-27 00:45:42 -0300

    funcionalidad lista

---
+++ main.js
@@ -0,0 +1,49 @@
+const container = document.getElementById("container");
+const obtenerInfoCards = () => { 
+  fetch("https://www.digi-api.com/api/v1/digimon") 
+    .then((res) => res.json()) 
+    .then((data) => renderDigimonCards(data)); 
+}
+  
+obtenerInfoCards(); 
+
+const renderDigimonCards = (cardsData) => { 
+  let digimonArray = cardsData.content;
+  container.innerHTML = "";
+  digimonArray.forEach((digimon) => {  
+    container.innerHTML += `
+      <div class="dgmn-card">
+        <img src="${digimon.image}" alt="">
+        <h2>${digimon.name}</h2>
+        <button onClick=viewDigimonDetail("${digimon.href}")>Ver Detalles</button>
+      </div>
+    `;
+  });
+};
+
+const viewDigimonDetail = (digimonUrl) => {
+  fetch(digimonUrl)
+  fetch(digimonUrl)
+    .then((res) => res.json()) 
+    .then((data) => {
+      const renderDescription = () => {
+        let englishDescription = "";
+        data.descriptions.forEach((description) => {
+          description.language === "en_us"
+            ? (englishDescription = description.description)
+            : "";
+        });
+        return englishDescription;
+      };
+      container.innerHTML = ""; 
+      container.innerHTML = `
+       <div class="card-detail">
+        <img src=${data.images[0].href} alt="">
+        <div class="card-detail__description">
+        <h1>${data.name}</h1>
+        <p>${renderDescription()}</p>
+        <button onclick="obtenerInfoCards()">Regresar</button>
+      </div>
+        </div>`;
+    });
+};
